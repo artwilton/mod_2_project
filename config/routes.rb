6ce19e9627d64c123bf0cf3b40ccfd7ee6324937
artwilton <Arthur.M.Wilton@gmail.com>
2020-10-22 17:32:53 -0400

    updated authentications, login, and signup

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
 
-  resources :users, only: [:create, :update, :destroy, :show]# do we still need show, or rename???
+  resources :users, only: [:update, :destroy, :show]# do we still need show, or rename???
   resources :lessons, except: :destroy
   get '/profile', to: 'users#profile'
   get '/profile/edit', to: 'users#edit'
